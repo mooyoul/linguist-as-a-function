db3aee4f5fe9ef47281f72b696908ff475d92f3c
MooYeol Prescott Lee <mooyoul@gmail.com>
2020-03-23 01:26:05 +0900

    cleanup

--- build-native-extensions.sh
+++ build-native-extensions.sh
@@ -4,5 +4,5 @@ mkdir /tmp/bin
 yum install -y cmake3 openssl-devel libicu-devel
 ln -s /usr/bin/cmake3 /tmp/bin/cmake
 bundle config set without 'test'
-bundle config set path 'vendor/bundler'
+bundle config set path 'vendor/bundle'
 env PATH=/tmp/bin:$PATH bundle install
