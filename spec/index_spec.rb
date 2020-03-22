2d5457e882dd8c73265c5b5755126e20b3005c70
MooYeol Prescott Lee <mooyoul@gmail.com>
2020-03-23 01:26:18 +0900

    cleanup

--- spec/index_spec.rb
+++ spec/index_spec.rb
@@ -2,15 +2,14 @@ require_relative "../index"
 
 describe LinguistHandler, "Handler" do
   context = {}
+  event = {
+    "name" => "test.sql",
+    "data" => "U0VMRUNUICogRlJPTSB0YWJsZSBXSEVSRSBpZCA9IDEyMzQ1",
+  }
 
   describe "#process" do
     context "with supported language" do
       it "should return detected language" do
-        event = {
-            "name" => "test.sql",
-            "data" => "SELECT * FROM table WHERE id = 123"
-        }
-
         result = LinguistHandler::Handler.process(event: event, context: context)
         expect(result).to eq({ name: "SQL" })
       end
@@ -28,10 +27,7 @@ describe LinguistHandler, "Handler" do
     context "with supported language" do
       it "should return detected language" do
         event = {
-          "body" => JSON.generate({
-            "name" => "test.sql",
-            "data" => "SELECT * FROM table WHERE id = 123"
-          }),
+          "body" => JSON.generate(event),
           "isBase64Encoded" => false,
         }
 
