a17121d7c368804ba808e6aef447f4e7e4284d58
MooYeol Prescott Lee <mooyoul@gmail.com>
2020-03-23 00:24:03 +0900

    fix ci

--- build-native-extensions.sh
+++ build-native-extensions.sh
@@ -2,5 +2,5 @@
 
 mkdir /tmp/bin
 yum install -y cmake3 openssl-devel libicu-devel
-ln -s /tmp/bin/cmake /usr/bin/cmake3
+ln -s /usr/bin/cmake3 /tmp/bin/cmake
 env PATH=/tmp/bin:$PATH bundle install --without test --path=vendor/bundle
